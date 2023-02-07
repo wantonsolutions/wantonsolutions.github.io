9d0d6b0ab141909daba2f8b1b7db8d44a7690957
wantonsolutions <stewbertgrant@gmail.com>
2023-02-07 14:53:07 -0800

    got snapping working for basic image

--- winnie_25/colors/index.js
+++ winnie_25/colors/index.js
@@ -1,10 +1,11 @@
 // Make the DIV element draggable:
-dragElement(document.getElementById("yellow_light"));
-dragElement(document.getElementById("red_light"));
-dragElement(document.getElementById("green_light"));
-dragElement(document.getElementById("magenta_light"));
-dragElement(document.getElementById("blue_light"));
-dragElement(document.getElementById("teal_light"));
+dragElement(document.getElementById("red_t_block"));
+// dragElement(document.getElementById("yellow_light"));
+// dragElement(document.getElementById("red_light"));
+// dragElement(document.getElementById("green_light"));
+// dragElement(document.getElementById("magenta_light"));
+// dragElement(document.getElementById("blue_light"));
+// dragElement(document.getElementById("teal_light"));
 
 
 function dragElement(elmnt) {
@@ -36,6 +37,7 @@ function dragElement(elmnt) {
     pos2 = pos4 - e.clientY;
     pos3 = e.clientX;
     pos4 = e.clientY;
+    // console.log(elmnt.style.left + "," + elmnt.style.top)
     // set the element's new position:
     elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
     elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
@@ -43,6 +45,20 @@ function dragElement(elmnt) {
 
   function closeDragElement() {
     // stop moving when mouse button is released:
+    first_box = document.getElementById("first_box");
+    box_size = first_box.offsetWidth;
+    baseline_y = first_box.offsetTop % box_size;
+    baseline_x = first_box.offsetLeft % box_size;
+    // console.log("Before: " +elmnt.style.left + "," + elmnt.style.top);
+    // console.log(elmnt.style.top)
+    // console.log(elmnt.offsetTop - pos2)
+    ysnap = (elmnt.offsetTop - pos2) % box_size;
+    xsnap = (elmnt.offsetLeft - pos1) % box_size;
+    elmnt.style.top = ((elmnt.offsetTop - pos2) - ysnap) + baseline_y +  "px";
+    elmnt.style.left = ((elmnt.offsetLeft - pos1) - xsnap) + baseline_x + "px";
+
+    // console.log("After: " + elmnt.style.left + "," + elmnt.style.top);
+
     document.onmouseup = null;
     document.onmousemove = null;
   }
